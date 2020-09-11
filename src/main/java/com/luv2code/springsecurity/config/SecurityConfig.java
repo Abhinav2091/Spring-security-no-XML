f98fcf9f9d62d112e409b7370ba1a35643d3eee0
abhinav2091 <abhinavsharma043@gmail.com>
2020-09-12 01:51:25 +0530

    Custom login Spring form with logout support

--- src/main/java/com/luv2code/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.luv2code.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -13,14 +14,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	//add our user for in memory Authentication
-		
+		// add our user for in memory Authentication
+
 		UserBuilder users = User.withDefaultPasswordEncoder();
-				auth.inMemoryAuthentication()
-				.withUser(users.username("Abhinav").password("root").roles("ADMIN"))
+		auth.inMemoryAuthentication().withUser(users.username("Abhinav").password("root").roles("ADMIN"))
 				.withUser(users.username("Aju").password("root").roles("EMPLOYEE"))
 				.withUser(users.username("Himanshu").password("root").roles("MANAGER"));
 	}
 
-	
+	// override the configure http method for custom login form.
+	//else remove it and use spring provided login form.
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//showMyLoginPage plain login page
+		//showMyFancyLoginPage fancy login page.
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/showMyFancyLoginPage")//my loginiN page
+				.loginProcessingUrl("/authenticateTheUser").permitAll()//check user name and pass provided by Spring.
+				.and().logout().permitAll();//adding log out support
+	}
+
 }
